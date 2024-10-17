08c1fa2340419053dec27616775f2159495d6db9
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-10-17 20:40:28 +0530

    add forgot password and reset password page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('forgot-password/', views.user_forgot_password, name='forgot-password'),
+    path('reset-password/', views.user_reset_password, name='reset-password'),
     path('register/', views.user_type, name='user-type'),
     path('register/user', views.user_register, name='register'),
     path('logout/', views.user_logout, name='logout'),
