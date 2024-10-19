e271f74d91e64dca60d7ee0815506ec808f22f36
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-10-19 17:31:25 +0530

    add threading to send email without disturbing ongoing process

--- email_services/views.py
+++ email_services/views.py
@@ -1,9 +1,10 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
+import threading
 
-def send_html_email(subject, recipient_email, context):
 
+def send_email(subject, recipient_email, context):
     # Render HTML email template
     html_content = render_to_string("emails/email_template.html", context)
 
@@ -18,3 +19,18 @@ def send_html_email(subject, recipient_email, context):
     # Set email to HTML content type
     email.content_subtype = "html"
     email.send()
+
+'''
+use Python's threading module to send emails in a separate thread.
+This allows the main thread to continue processing requests without waiting for the email to send.
+'''
+
+def send_html_email(subject, recipient_email, context):
+
+    # Start a new thread to send email.
+    thread = threading.Thread(
+        target=send_email, args=(subject, recipient_email, context)
+    )
+    thread.start()
+
+
